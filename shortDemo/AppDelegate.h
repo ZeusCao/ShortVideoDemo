ec22cbc401f81f69f9bd0d071b9ddc783d8ce7ed
曹檑 <1090491594@qq.com>
2017-10-10 17:00:41 +0800

    Initial Commit

---
+++ shortDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shortDemo
+//
+//  Created by Zeus on 2017/10/10.
+//  Copyright © 2017年 Zeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
